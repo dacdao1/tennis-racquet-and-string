b792f7cb4230494e3ac806d0c21cb97c842d5e4e
dacdao1 <dacdao01@gmail.com>
2020-02-25 15:30:49 -0500

    adding racquet images

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,103 @@
 <template>
 <div id="App">
 
-    <b-form-input type="text" v-model="userInput" v-on:keyup.enter="getRacquet()"placeholder="Enter racquet name"></b-form-input>
-<ul>
-  <li v-for="racquet in racquets" style="margin-top: 10px">{{racquet}}</li>
+  <b-form-input type="text" v-model="userInput" v-on:keyup.enter=" userInput.length <= 4 ? removeEverything(): getRacquet()" placeholder="Enter racquet name"></b-form-input>
+  <img class="brandImage" :src="brandImage" />
 
+  <ul>
+    <li v-for="racquet in racquets" style="margin-top: 10px">{{racquet}}</li>
 
-</ul>
+  </ul>
 </div>
 </template>
 
 <script>
 export default {
   name: 'App',
-  data () {
+  data() {
     return {
-      userInput:'',
-      result:'',
+      userInput: '',
+      result: '',
       result1: '',
       racquets: {},
+      brandImage: " ",
+
     }
   },
   methods: {
     //video preview of each racquet or string through Youtube, based on user input
     //function that will retrieve strings based on user selection
     //function that will retrieve racquets based on user selection
-    getRacquet () {
+    removeEverything() {
+      this.brandImage = " "
+      this.raccquets = {}
+    },
+    getRacquet() {
 
 
-// This function goal is to return the user input into uppercase for each word
-  var upperCaseThis = this.userInput.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 
 
-//Filter the user input to see if they typed in any of the brand name
+      //Filter the user input to see if they typed in any of the brand name
 
-//Fiter the user input to see if they typed in any of the model name
+      //Fiter the user input to see if they typed in any of the model name
 
 
-//calling the api of racketlogger for all of the racquet and also string
-        function getRacquetName(){
-          return new Promise((resolve, reject)=>{
-            fetch("https://api.racketlogger.com/rackets/",{
-              headers:{
-                "User-Agent": "Awesome-Tennis-App"
-              }
-            }).then(response=>{return response.json()}).then(data=>{resolve(data)}).catch(err=>{reject(err)})
+      //calling the api of racketlogger for all of the racquet and also string
+      function getRacquetName() {
+        return new Promise((resolve, reject) => {
+          fetch("https://api.racketlogger.com/rackets/", {
+            headers: {
+              "User-Agent": "Awesome-Tennis-App"
+            }
+          }).then(response => {
+            return response.json()
+          }).then(data => {
+            resolve(data)
+          }).catch(err => {
+            reject(err)
           })
-        }
-
-  getRacquetName().then(data=>{
-  const arr3 = data.filter(x => x.model.toLowerCase().includes(this.userInput.toLowerCase()))
-//Pure Drive 2015
-//pure drive 2015
-
-    this.racquets = arr3
-    console.log(JSON.stringify(arr3))
-
-    // this.result1 = arr1
-  }).catch(err=>{console.log(err)})
-
+        })
       }
+
+      getRacquetName().then(data => {
+        const arr3 = data.filter(x => x.model.toLowerCase().match(this.userInput.toLowerCase()))
+
+        var brandName = arr3[0].brand.toLowerCase()
+        switch (brandName) {
+          case "babolat":
+            this.brandImage = require('./assets/Babolat.jpg')
+            break;
+          case "head":
+            this.brandImage = require('./assets/Head.jpg')
+            break;
+          case "prince":
+            this.brandImage = require('./assets/prince.png')
+            break;
+          case "wilson":
+            this.brandImage = require('./assets/wilson.png')
+            break;
+          case "yonex":
+            this.brandImage = require('./assets/yonex.png')
+            break;
+          default:
+            this.brandImage = ''
+        }
+        this.racquets = arr3
+
+
+
+
+        // this.result1 = arr1
+      }).catch(err => {
+        console.log(err)
+      })
+
+
+
+      // This function goal is to return the user input into uppercase for each word
+
     }
+  }
 
 
 }
