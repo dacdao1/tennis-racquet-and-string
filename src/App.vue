51f16b38bb88da1fab7d21728a243dbcac4d8d26
dacdao1 <dacdao01@gmail.com>
2020-02-27 17:18:11 -0500

    Added card for each racquets

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,24 @@
   <b-form-input type="text" v-model="userInput" v-on:keyup.enter=" userInput.length <= 4 ? removeEverything(): getRacquet()" placeholder="Enter racquet name"></b-form-input>
   <img class="brandImage" :src="brandImage" />
 
-  <ul>
-    <li v-for="racquet in racquets" style="margin-top: 10px">{{racquet}}</li>
 
-  </ul>
+  <!-- <ul>
+    <li v-for="racquet in racquets" style="margin-top: 10px">{{racquet["Skip M Holes"]}} , {{racquet["Start C"]}}</li>
+
+  </ul> -->
+  <div v-for="racquet in racquets">
+    <b-card v-model:title="racquet" Racquet Name "]" img-src=" https://picsum.photos/600/300/?image=25" img-alt="Image" img-top tag="article" style="max-width: 20rem;" class="mb-2">
+      <b-card-text>
+        {{racquet}}
+      </b-card-text>
+
+      <b-button href="#" variant="primary">Go somewhere</b-button>
+    </b-card>
+
+  </div>
+
+
+  <p>{{result1}}</p>
 </div>
 </template>
 
@@ -17,7 +31,6 @@ export default {
   data() {
     return {
       userInput: '',
-      result: '',
       result1: '',
       racquets: {},
       brandImage: " ",
@@ -25,9 +38,7 @@ export default {
     }
   },
   methods: {
-    //video preview of each racquet or string through Youtube, based on user input
-    //function that will retrieve strings based on user selection
-    //function that will retrieve racquets based on user selection
+
     removeEverything() {
       this.brandImage = " "
       this.raccquets = {}
@@ -38,6 +49,7 @@ export default {
       if (this.userInput.toLowerCase().includes("babolat")) {
         var modelName = this.userInput.toLowerCase().replace("babolat ", "")
         var brandName = "Babolat"
+        this.brandImage = require('./assets/Babolat.jpg')
       } else if (this.userInput.toLowerCase().includes("asics")) {
         var modelName = this.userInput.toLowerCase().replace("asics ", "")
         var brandName = "Asics"
@@ -84,23 +96,14 @@ export default {
         var modelName = this.userInput.toLowerCase().replace("yonex ", "")
         var brandName = "Yonex"
       } else {
-        console.log(this.userInput)
+        this.result1 = "please enter a brand name in the search box"
       }
 
 
-      //Filter the user input to see if they typed in any of the brand name
 
-      //Fiter the user input to see if they typed in any of the model name
-
-
-      //calling the api of racketlogger for all of the racquet and also string
       function getRacquetName(brandName) {
         return new Promise((resolve, reject) => {
-          fetch("https://racquet-stringing-52165.firebaseio.com/" + brandName + ".json", {
-            headers: {
-              "User-Agent": "Awesome-Tennis-App"
-            }
-          }).then(response => {
+          fetch("https://racquet-stringing-52165.firebaseio.com/" + brandName + ".json").then(response => {
             return response.json()
           }).then(data => {
             resolve(data)
@@ -112,38 +115,10 @@ export default {
 
       getRacquetName(brandName).then(data => {
 
-
-
         const arr3 = data.filter(x => x["Racquet Name"].toLowerCase().includes(modelName.toLowerCase()))
-        console.log(arr3)
+
         this.racquets = arr3
-        //
-        // var brandName = arr3[0].brand.toLowerCase()
-        // switch (brandName) {
-        //   case "babolat":
-        //     this.brandImage = require('./assets/Babolat.jpg')
-        //     break;
-        //   case "head":
-        //     this.brandImage = require('./assets/Head.jpg')
-        //     break;
-        //   case "prince":
-        //     this.brandImage = require('./assets/prince.png')
-        //     break;
-        //   case "wilson":
-        //     this.brandImage = require('./assets/wilson.png')
-        //     break;
-        //   case "yonex":
-        //     this.brandImage = require('./assets/yonex.png')
-        //     break;
-        //   default:
-        //     this.brandImage = ''
-        // }
-        // this.racquets = arr3
 
-
-
-
-        // this.result1 = arr1
       }).catch(err => {
         console.log(err)
       })
